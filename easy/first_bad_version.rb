451f4ed38e531c7af16d3a332ffaaf9056c79a29
Caleb KM <calebkm@gmail.com>
2022-05-23 16:47:26 -0700

    Update first_bad_version.rb

--- easy/first_bad_version.rb
+++ easy/first_bad_version.rb
@@ -1,15 +1,16 @@
 # LeetCode - First Bad Version
 # https://leetcode.com/problems/first-bad-version
 
-def first_bad_version(n, max=nil)
+def first_bad_version(n, min=0, max=nil)
   max ||= n
-  guess = (max + n) / 2
   
   if is_bad_version(n)
     return n unless is_bad_version(n-1)
     max = n
-    guess = n / 2
+  else
+    min = n
   end
   
-  first_bad_version(guess, max)
+  n = (max + min) / 2
+  first_bad_version(n, min, max)
 end
